0e9d2efa6883ea2d84682dcb883996c877830a36
JesusDFabra <131053445+JesusDFabra@users.noreply.github.com>
2023-08-31 14:36:51 -0500

    Add files via upload
    Cambio de notas y octavas listo. (Se agregaron los botones para cambio de octava)

--- DrumPad_1.ino
+++ DrumPad_1.ino
@@ -7,22 +7,27 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 //ENDODER
 #define A 2 //CLK
 #define B 3 //DT
-#define botonUP   12 //CLK
-#define botonDown 11 //DT
-int KEY_ANT = 0;		
-//VARIABLES MIDI
-volatile int KEY = 0;// 0 - 255, 0 = C-2 (Do -2)
+#define boton1 12 //Up
+#define boton2 11 //Down
+int KEY_ANT = 0;	//Guarda el key anterior
+//VARIABLES
+volatile int KEY = 0;// 0 - 255, 0 = C-2 (Do -2) //volatile, que se usa en el atach
 String Notas[12] = {"C ", "C#", "D ", "D#", "E ", "F ", "F#", "G ", "G#", "A ", "A#", "B "};
 String NOTA =  ""; 
 int OCTAVA = -2; //DE -2 A 8
 
+//Auxiliares
+bool botPulsado = false;
 //////////  SETUP   ////////////
 void setup() {
   //INICIALIZACIÓN
   lcd.begin(16, 2);
   
-  pinMode(A, INPUT);		// A como entrada
-  pinMode(B, INPUT);		// B como entrada
+  pinMode(A, INPUT);		  // A como entrada
+  pinMode(B, INPUT);		  // B como entrada
+  pinMode(boton1, INPUT);
+  pinMode(boton2, INPUT); 
+
   attachInterrupt(digitalPinToInterrupt(A), encoder, LOW);// interrupcion sobre pin A con low
   //START
   lcd.clear();
@@ -32,19 +37,26 @@ void setup() {
 }
 ////////////  LOOP  //////////////
 void loop() {
-  //Para que cuando pase de 10 a 9 o de 100 a 99 no se quede el 0 pintado
-    if (KEY != KEY_ANT) {	
-      if(KEY_ANT == 10 && KEY == 9){
-        lcd.setCursor(4, 1);
-        lcd.print(" ");
-      }
-      else if (KEY_ANT == 100 && KEY == 99){
-        lcd.setCursor(5, 1);
-        lcd.print(" ");      
-      }
-    KEY_ANT = KEY ;	// asigna a ANTERIOR el valor actualizado de POSICION
+
+  //Botones 1 y 2, detección de pulso.
+  if(digitalRead(boton1) == HIGH  && botPulsado == false){
+    if (KEY <= 115){KEY = KEY + 12;}
+    else{KEY = 127;}
+    LCD_refresh();
+    botPulsado = true;
+  }
+  else if(digitalRead(boton2) == HIGH && botPulsado == false){
+    if (KEY >= 12){KEY = KEY - 12;}
+    else{KEY = 0;} 
+    LCD_refresh();
+    botPulsado = true;
+  }
+  else if (digitalRead(boton2) == LOW && digitalRead(boton1) == LOW && botPulsado == true){
+    botPulsado = false;
   }
 }
+
+
 ////////////  FUNCIONES ///////////////
 void encoder()  {
   static unsigned long ultimaInterrupcion = 0;
@@ -58,21 +70,10 @@ void encoder()  {
       KEY--;
     }
     KEY = min(127, max(0, KEY));	// 0-127 
-
-    NOTA = Notas[KEY%12];
-    OCTAVA = (KEY / 12) - 2;
-
     ultimaInterrupcion = tiempoInterrupcion;
     
-    lcd.setCursor(3, 0);
-    lcd.print(NOTA);
-    
-    lcd.setCursor(5, 0);
-    lcd.print("  ");
-    lcd.setCursor(5, 0);
-    lcd.print(OCTAVA);
-    lcd.setCursor(3, 1);
-    lcd.print(KEY);
+
+    LCD_refresh();
 
   }
 }
@@ -90,5 +91,31 @@ void pantalla(){
   lcd.print(" K ");
   lcd.setCursor(3, 1);
   lcd.print(KEY);
+}
+
+void LCD_refresh(){
+    NOTA = Notas[KEY%12];
+    OCTAVA = (KEY / 12) - 2;
+    
+    lcd.setCursor(3, 0);
+    lcd.print(NOTA);
+    lcd.setCursor(5, 0);
+    lcd.print("  ");
+    lcd.setCursor(5, 0);
+    lcd.print(OCTAVA);
+    lcd.setCursor(3, 1);
+    lcd.print(KEY);
+
+    if (KEY != KEY_ANT) {	
+      if(KEY_ANT >= 10 && KEY < 10){
+        lcd.setCursor(4, 1);
+        lcd.print(" ");
+      }
+      else if (KEY_ANT >= 100 && KEY < 100){
+        lcd.setCursor(5, 1);
+        lcd.print(" ");      
+      }
+    KEY_ANT = KEY ;	// asigna a ANTERIOR el valor actualizado de POSICION
+  }
 
 }
